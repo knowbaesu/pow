a1b45463f3b56b1d90950f34c06a5f219ea55523
pjs <vkrwltn12@gmail.com>
2023-03-18 22:29:59 +0900

    pra1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,40 @@
-import java.util.Calendar;
+package prac;
+
 import java.util.Scanner;
 
-public class JAVA04 {
-
+public class Java_if_prac {
     public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        System.out.println(cal.getTime());
-        System.out.println(cal.get(Calendar.YEAR));
-        System.out.println(cal.get(Calendar.MONTH));
-        System.out.println(cal.get(Calendar.DATE));
-        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-        int year = 2020;
-        int month = Calendar.FEBRUARY;
-        int date = 1;
-
-
-        cal.set(year, month, date);
-
-        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-        int maxDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        System.out.println(cal.get(Cal));
-//		int b = cal.get(Calendar.YEAR);
-//		int c = b+1;
-//		System.out.println(c);
-//
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int b = scan.nextInt();
 
 
+        System.out.println("첫번째 숫자를 입력하세요");
+        int num_1 = scan.nextInt();
+
+        System.out.println("두번째 숫자를 입력하세요");
+        int num_2 = scan.nextInt();
+
+        System.out.println("세번째 숫자를 입력하세요");
+        int num_3 = scan.nextInt();
+
+
+        if(num_1>num_2) {
+            if(num_1>num_3) {
+                System.out.println("가장 큰 수는 : " + num_1+"입니다");
+            }
+        }
+        if(num_2>num_1) {
+            if(num_2 > num_3) {
+                System.out.println("가장 큰 수는:" +num_2 +"입니다" );
+            }
+        }
+
+        if(num_3 > num_1 ) {
+            if(num_3 > num_2 ) {
+                System.out.println("가장 큰 수는:" +num_3 +"입니다" );
+            }
+        }
+        scan.close();
     }
 
 }
+
